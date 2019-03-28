11675066d17f5f8bba591d3b0937d7657039bb39
J M <noreply.johann.misterio@gmail.com>
2019-03-27 22:31:52 -0400

    obtain a collection per node asana documentation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,21 @@ import * as asanaSecrets from '../configurations/asana/private/auth'
 })
 export class AppComponent {
   title = 'taskboard141';
+
+  results: Object;
+  ngOnInit() {
+    this.getResults()
+  }
+
+  getResults() {
+    const asana = require('node_modules/asana/dist/asana-min.js')
+    const client = asana.Client.create().useAccessToken(asanaSecrets.my_access_token);
+
+    client.tasks.findByProject(asanaSecrets.projectID, 'opt_expand=memberships,opt_fields=resource_subtype').then(collection => {
+      collection.fetch(200).then((tasks: any) => {
+        this.results = tasks;
+        console.log(this.results)
+      });
+    });
+  }
 }
