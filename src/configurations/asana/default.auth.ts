e56cfe355a385a5f6a483d2619a0e9623f57c9e8
J M <noreply.johann.misterio@gmail.com>
2019-03-27 21:56:27 -0400

    created a folder structure to store auth details

---
+++ src/configurations/asana/default.auth.ts
@@ -0,0 +1,9 @@
+// Rename this file to "asana.ts", move to folder called "private" and populate accordingly //
+
+'use strict';
+
+// Asana access token
+export const my_access_token = '';                        
+
+// Asana project id
+export const projectID = '';                             
